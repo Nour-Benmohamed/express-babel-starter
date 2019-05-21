af14c97ed876b358be1e1128d7dcc097e0d7bd04
Nour Benmohamed <nour.benmohamed.21@dartmouth.edu>
2019-05-20 23:46:57 -0400

    EC8

---
+++ src/models/note_model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from 'mongoose';
+
+// create a PostSchema with a title field
+
+const NoteSchema = new Schema({
+    title: String,
+    x: Number,
+    y: Number,
+    zIndex: Number,
+    text: String,
+  }, {
+    toJSON: {
+      virtuals: true,
+    },
+  });
+  
+  const NoteModel = mongoose.model('Note', NoteSchema);
+  // create PostModel class from schema
+  
+  
+  export default NoteModel;
